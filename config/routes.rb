234048274efda1a00023a8e73d530691ffce5e1f
Ryker Reed <ryker.reed3@gmail.com>
2017-09-17 01:40:42 -0400

    Makes landing page the boards index. Adds code to display all board names in index.

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
       resources :cards
     end
   end
+
+  root "boards#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
