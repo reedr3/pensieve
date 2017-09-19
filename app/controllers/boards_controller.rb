9470fbb4dcf072aa9e75fb46cbf2bfb1c939c020
Ryker Reed <ryker.reed3@gmail.com>
2017-09-19 16:11:53 -0400

    Adds new and post to boards controller and creates corresponding view.

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -7,4 +7,22 @@ class BoardsController < ApplicationController
     @board = Board.find(params[:id])
     @lists = List.all
   end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    @board = Board.new(board_params)
+    if @board.save
+      redirect_to boards_path
+    end
+  end
+
+private
+
+def board_params
+  params.require(:board).permit(:name, :color)
+end
+
 end
