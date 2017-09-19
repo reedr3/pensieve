5bf341332e79aad6b0618896ee20c941a87011e4
Ryker Reed <ryker.reed3@gmail.com>
2017-09-19 16:10:45 -0400

    Adds components for the basic pieces of the app.

--- app/assets/javascripts/components/board.js.jsx
+++ app/assets/javascripts/components/board.js.jsx
@@ -1,1 +1,8 @@
 // individual board display, with lists, each list having cards
+var Board = React.createClass({
+  render: function() {
+    return (
+      <div></div>
+    );
+  }
+});
--- app/assets/javascripts/components/boards.js.jsx
+++ app/assets/javascripts/components/boards.js.jsx
@@ -1,1 +1,13 @@
 // list of all boards as clickable links to their board pages
+var Boards = React.createClass({
+  render: function() {
+    return (
+      <div id="boards">
+        {this.props.boards.map(function(board) {
+          return <div className="board-in-index-view"> <a href={"/boards/" + board.id}> <p> {board.name} </p> </a> </div>
+        }.bind(this))}
+        <div className="board-in-index-view"> <a href="/boards/new"> <p> Create new board </p> </a> </div>
+      </div>
+    );
+  }
+});
