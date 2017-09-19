4a2513c2ed994344ecca609d6d1f74712d2ce292
Ryker Reed <ryker.reed3@gmail.com>
2017-09-19 17:02:01 -0400

    Adds edit, update, and delete to boards controller. Adds corresponding views and links.

--- app/assets/javascripts/components/board.js.jsx
+++ app/assets/javascripts/components/board.js.jsx
@@ -2,7 +2,16 @@
 var Board = React.createClass({
   render: function() {
     return (
-      <div></div>
+      <div>
+        <h1>{this.props.board.name}</h1>
+        <p>{this.props.board.color}</p>
+        <a href={"/boards/" + this.props.board.id + "/edit"}>Edit Board</a>
+        <form accept-charset="UTF-8" action={"/boards/" + this.props.board.id} method="post">
+          <input name="_method" type="hidden" value="delete" />
+          <input name="authenticity_token" type="hidden" value={this.props.authenticity_token} />
+          <input type="submit" value="Delete Board" />
+        </form>
+      </div>
     );
   }
 });
--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -16,8 +16,31 @@ class BoardsController < ApplicationController
     @board = Board.new(board_params)
     if @board.save
       redirect_to boards_path
+    else
+      redirect_to new_board_path
     end
   end
+
+  def edit
+    @board = Board.find(params[:id])
+  end
+
+  def update
+    @board = Board.find(params[:id])
+    if @board.update(name: params[:name], color: params[:color])
+      redirect_to board_path(@board)
+    else
+      redirect_to edit_board_path
+    end
+  end
+
+  def destroy
+    @board = Board.find(params[:id])
+
+		@board.destroy
+		redirect_to boards_path
+  end
+
 
 private
 
