f62c2cd1ad9543856c0c323e23f5b851778dc002
Ryker Reed <ryker.reed3@gmail.com>
2017-09-19 19:37:10 -0400

    Adds user id to each board. Makes boards only viewable by user they belong to.

--- db/migrate/20170917043928_create_boards.rb
+++ db/migrate/20170917043928_create_boards.rb
@@ -3,6 +3,7 @@ class CreateBoards < ActiveRecord::Migration[5.1]
     create_table :boards do |t|
       t.string :name
       t.string :color
+      t.integer :user_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170919212215) do
   create_table "boards", force: :cascade do |t|
     t.string "name"
     t.string "color"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
