5e680cd1e2bc4c09c5c222b7f2c8eacbfef1e81e
Ryker Reed <ryker.reed3@gmail.com>
2017-09-23 00:24:38 -0400

    Adds css styling, navigation buttons, and moves board and boards components inside of container components.

--- app/assets/javascripts/components/main_header.js.jsx
+++ app/assets/javascripts/components/main_header.js.jsx
@@ -3,9 +3,10 @@ var MainHeader = React.createClass({
     return (
       <header id="main-header">
         <div className="container">
-          <img src="" alt="Pensieve Logo"></img>
+          <img src="https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21728061_10156505665328998_5498779670418140584_n.jpg?oh=b2e71626e03fa2729df45e23efe13a76&oe=5A5DBBC9" alt="Pensieve Logo"></img>
+          <p>Logged in as {this.props.user.email}</p>
           <a className="main-header-button" href="/users/edit">Profile</a>
-          <form accept-charset="UTF-8" action={"/sessions/" + this.props.user_id} method="post">
+          <form accept-charset="UTF-8" action={"/users/sign_out"} method="post">
             <input name="_method" type="hidden" value="delete" />
             <input name="authenticity_token" type="hidden" value={this.props.authenticity_token} />
             <input type="submit" className="main-header-button" value="Logout" />
