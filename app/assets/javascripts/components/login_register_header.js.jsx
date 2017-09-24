b8a37b770ee9fd584ace21c26e0ac02adc82657b
Ryker Reed <ryker.reed3@gmail.com>
2017-09-24 17:00:57 -0400

    Finishes basic styling for headers and login and register pages.

--- app/assets/javascripts/components/login_register_header.js.jsx
+++ app/assets/javascripts/components/login_register_header.js.jsx
@@ -4,8 +4,8 @@ var LoginRegisterHeader = React.createClass({
       <header id="login-register-header">
         <div className="container">
           <img src="https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21728061_10156505665328998_5498779670418140584_n.jpg?oh=b2e71626e03fa2729df45e23efe13a76&oe=5A5DBBC9" alt="Pensieve Logo"></img>
-          <div id="login-register-buttons">
-            <a className="login-register-button button" href="/users/sign_in">Login</a>
+          <div id="login-register-header-buttons">
+            <a className="login-register-button button" href="/users/sign_in">Log in</a>
             <a className="login-register-button button" href="/users/sign_up">Register</a>
           </div>
         </div>
--- app/assets/javascripts/components/main_header.js.jsx
+++ app/assets/javascripts/components/main_header.js.jsx
@@ -5,12 +5,14 @@ var MainHeader = React.createClass({
         <div className="container">
           <img src="https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21728061_10156505665328998_5498779670418140584_n.jpg?oh=b2e71626e03fa2729df45e23efe13a76&oe=5A5DBBC9" alt="Pensieve Logo"></img>
           <p>Logged in as {this.props.user.email}</p>
-          <a className="main-header-button button" href="/users/edit">Profile</a>
-          <form accept-charset="UTF-8" action={"/users/sign_out"} method="post">
-            <input name="_method" type="hidden" value="delete" />
-            <input name="authenticity_token" type="hidden" value={this.props.authenticity_token} />
-            <input type="submit" className="main-header-button button" value="Logout" />
-          </form>
+          <div id="main-header-buttons">
+            <a className="main-header-button button" href="/users/edit">Profile</a>
+            <form accept-charset="UTF-8" action={"/users/sign_out"} method="post">
+              <input name="_method" type="hidden" value="delete" />
+              <input name="authenticity_token" type="hidden" value={this.props.authenticity_token} />
+              <input type="submit" className="main-header-button button" value="Logout" />
+            </form>
+          </div>
         </div>
       </header>
     );
