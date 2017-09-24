d4ca6c494949c23f278c9a4e9beb42f639b42d4f
Ryker Reed <ryker.reed3@gmail.com>
2017-09-24 18:25:45 -0400

    Adds components for list resource.

--- app/assets/javascripts/components/main_content/list.js.jsx
+++ app/assets/javascripts/components/main_content/list.js.jsx
@@ -1,7 +1,16 @@
 var List = React.createClass({
   render: function() {
     return (
-      <div></div>
+      <div className="list">
+        <h1>{this.props.listName}</h1>
+
+        <a href={"/boards/" + this.props.board.id + "/lists/" + this.props.listId + "/edit"}>Edit List</a>
+        <form accept-charset="UTF-8" action={"/boards/" + this.props.board.id + "/lists/" + this.props.listId} method="post">
+          <input name="_method" type="hidden" value="delete" />
+          <input name="authenticity_token" type="hidden" value={this.props.authenticityToken} />
+          <input type="submit" value="Delete List" />
+        </form>
+      </div>
     );
   }
 });
