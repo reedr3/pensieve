c8b51b00751138adea7be182007d0a4d943d2b19
Ryker Reed <ryker.reed3@gmail.com>
2017-09-28 00:41:39 -0400

    Adds pensieve logo to headers and adds favicon for browser tab.

--- app/assets/javascripts/components/login_register_header.js.jsx
+++ app/assets/javascripts/components/login_register_header.js.jsx
@@ -3,7 +3,7 @@ var LoginRegisterHeader = React.createClass({
     return (
       <header id="login-register-header">
         <div className="container">
-          <img src="https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21728061_10156505665328998_5498779670418140584_n.jpg?oh=b2e71626e03fa2729df45e23efe13a76&oe=5A5DBBC9" alt="Pensieve Logo"></img>
+          <img src="https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/22007520_10156579022983998_4159538439274514533_n.jpg?oh=d1195a772d61946195e4e4e2c30686cc&oe=5A4AD5E0" alt="Pensieve Logo"></img>
           <div id="login-register-header-buttons">
             <a className="login-register-button button" href="/users/sign_in">Log in</a>
             <a className="login-register-button button" href="/users/sign_up">Register</a>
--- app/assets/javascripts/components/main_header.js.jsx
+++ app/assets/javascripts/components/main_header.js.jsx
@@ -3,7 +3,7 @@ var MainHeader = React.createClass({
     return (
       <header id="main-header">
         <div className="container">
-          <a href="/boards"> <img id="logo" src="https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/21728061_10156505665328998_5498779670418140584_n.jpg?oh=b2e71626e03fa2729df45e23efe13a76&oe=5A5DBBC9" alt="Pensieve Logo"></img> </a>
+          <a href="/boards"> <img id="logo" src="https://scontent.fbed1-1.fna.fbcdn.net/v/t1.0-9/22007520_10156579022983998_4159538439274514533_n.jpg?oh=d1195a772d61946195e4e4e2c30686cc&oe=5A4AD5E0" alt="Pensieve Logo"></img> </a>
 
           <Dropdown path={"/users"} menuNoun="Profile" backgroundColor="#222" authenticityToken={this.props.authenticity_token}/>
 
